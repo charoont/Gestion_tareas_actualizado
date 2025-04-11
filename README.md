deea2e6183b4417d5fcac766a8b853bb3ce4f13b
Camilo Rojas <jrojascamilo22@gmail.com>
2025-04-11 09:49:09 -0500

    Actualizacion: ajustes readme, urls.py

---
+++ README.md
@@ -0,0 +1,19 @@
+# 📝 Trello Básico API - Django REST Framework
+
+Una API que simula la lógica de un Trello, construida con Django REST Framework, para gestionar tareas y flujos de trabajo.
+
+## 🚀 Características
+
+- CRUD completo de tareas
+- Asignación de usuarios
+- Filtros por estado, usuario y fecha
+- Autenticación integrada
+- Documentación Swagger
+
+## 🧩 Tecnologías
+
+- Django 5.1
+- Django REST Framework
+- drf-yasg
+- django-filter
+- PostgreSQL
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,32 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tareas.views import TareaViewSet
-from django.contrib import admin
 
+# Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Configuración del esquema de Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Trello API",
+        default_version='v1',
+        description="Documentación de la API de tareas estilo Trello",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Rutas API
 router = DefaultRouter()
 router.register(r'tareas', TareaViewSet)
+
+# URLs
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),  
+    path('admin/', admin.site.urls), 
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger
 ]
 
